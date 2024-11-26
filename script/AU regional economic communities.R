d8808d6d1594beb7336d1df21a4e047752af799d
liuyanguu <lyhello@gmail.com>
2024-11-26 10:37:38 -0500

    revise to AUREC

--- script/AU regional economic communities.R
+++ script/AU regional economic communities.R
@@ -9,14 +9,14 @@ dir_project <- here::here() # Set the working directory to Country-and-Region-Me
 # Create the data frame
 dt_rec <- data.table(
   Region = c(
-    rep("Arab Maghreb Union (UMA)", 5),
-    rep("Common Market for Eastern and Southern Africa (COMESA)", 19),
-    rep("Community of Sahel-Saharan States (CEN-SAD)", 29),
-    rep("East African Community (EAC)", 5),
-    rep("Economic Community of Central African States (ECCAS)", 10),
-    rep("Economic Community of West African States (ECOWAS)", 15),
-    rep("Intergovernmental Authority on Development (IGAD)", 8),
-    rep("Southern African Development Community (SADC)", 15)
+    rep("Arab Maghreb Union (UMA)", 5), # 1
+    rep("Common Market for Eastern and Southern Africa (COMESA)", 19), # 2
+    rep("Community of Sahel-Saharan States (CEN-SAD)", 29), # 3
+    rep("East African Community (EAC)", 5), # 1
+    rep("Economic Community of Central African States (ECCAS)", 10), # 4
+    rep("Economic Community of West African States (ECOWAS)", 15), # 5
+    rep("Intergovernmental Authority on Development (IGAD)", 8), # 4
+    rep("Southern African Development Community (SADC)", 15) # 5 
   ),
   Country = c(
     # UMA
@@ -51,11 +51,12 @@ dt_rec <- data.table(
 )
 
 # Extract shortnames from the Community column
-dt_rec$Region_Code <- paste0("AU_", sub(".*\\((.*)\\).*", "\\1", dt_rec$Region))
+dt_rec$Region_Code <- paste0("AUREC_", sub(".*\\((.*)\\).*", "\\1", dt_rec$Region))
 dt_rec$ISO3Code <- countrycode::countrycode(dt_rec$Country, origin = "country.name", destination = "iso3c")
-dt_rec$Regional_Grouping <- "AU" 
+dt_rec$Regional_Grouping <- "AUREC" 
 dt_rec <- dt_rec[,.(Regional_Grouping, Region, Region_Code, Country, ISO3Code)]
 head(dt_rec)
 table(dt_rec$Region)
 # Save the data to output folder 
 fwrite(dt_rec, "output/African Union/AU_regional economic communities.csv")
+
--- script/HAC 2025.R
+++ script/HAC 2025.R
@@ -1,19 +1,13 @@
 library("data.table")
 dt_lb <- CME.assistant::get.live.birth(2024)
-dt_lb <- dt_lb[sex == "b" & year == 2024.5]
+dt_lb[, year := floor(year)]
+dt_lb <- dt_lb[sex == "b" & year %in% c(2024, 2025)]
+dt_lbw <- dcast(dt_lb, ISO3Code ~ year, value.var = "lb")
+setnames(dt_lbw, c("2024", "2025"), c("lb2024", "lb2025"))
 
-dc <- setDT(readxl::read_xlsx("D:/OneDrive - UNICEF/Documents - Child Mortality/UN IGME data/update country.info.CME/Regional Groupings/2024/UNICEF Programme Countries and Territories.xlsx"))
+workdir <- "D:/OneDrive - UNICEF/Documents - Child Mortality/UN IGME data/update country.info.CME/"
 
-dc[, `:=`(HAC = `2023 Countries with Stand-alone HACs or Multi-country HACs with ORE over 50%`, 
-          Humanitarian = `Humanitarian countries for impact and outcome indicators`)]
-
-iso_HAC_2023 <- dc[HAC == "Yes", `ISO3 Code`]
-dt_lb[ISO3Code %in% dc[HAC == "Yes", `ISO3 Code`], sum(lb)]
-dt_lb[ISO3Code %in% dc[Humanitarian == "Yes", `ISO3 Code`], sum(lb)]
-
-lbtotal <- sum(dt_lb$lb)
-
-dcHAC <- setDT(readxl::read_xlsx("D:/OneDrive - UNICEF/Documents - Child Mortality/UN IGME data/update country.info.CME/Regional Groupings/2024/Provisional HAC 2025 Appeals_for DGCA 21Nov2024.xlsx",
+dcHAC <- setDT(readxl::read_xlsx(file.path(workdir, "Country-and-Region-Metadata/raw_data/UNICEF_SP/Provisional HAC 2025 Appeals_for DGCA 21Nov2024.xlsx"),
                               sheet = "HAC 2025 coverage", skip = 1))
 setnames(dcHAC, c("Region", "Country", "N", "Type"))
 dcHAC <- dcHAC[Type!='Regional']
@@ -22,14 +16,10 @@ dcHAC <- dcHAC[!is.na(iso3)]
 dcHAC[,.N]
 dcHAC[, table(Type)]
 iso_HAC_2024 <- dcHAC$iso3
-setdiff(iso_HAC_2024, iso_HAC_2023)
 
-countrycode::countrycode(setdiff(iso_HAC_2023, iso_HAC_2024), origin = "iso3c", destination = "country.name")
-countrycode::countrycode(setdiff(iso_HAC_2024, iso_HAC_2023), origin = "iso3c", destination = "country.name")
+dt_lbw[ISO3Code %in% dcHAC$iso3, sum(lb2024)] # 57570251
+dt_lbw[ISO3Code %in% dcHAC$iso3, sum(lb2025)] # 57879887
 
-dt_lb[ISO3Code %in% dcHAC$iso3, sum(lb)] # 57570251
-
-dcHAC <- dplyr::left_join(dcHAC, dt_lb[, .(ISO3Code, lb)], by = c("iso3" = "ISO3Code"))
-setnames(dcHAC, "lb", "lb2024")
-dcHAC[, Region := Region[!is.na(Region)][cumsum(!is.na(Region))]]
-writexl::write_xlsx(dcHAC, "HAC2024 with live birth.xlsx")
+dcHAC_lb <- dplyr::left_join(dcHAC, dt_lbw, by = c("iso3" = "ISO3Code"))
+dcHAC_lb[, Region := Region[!is.na(Region)][cumsum(!is.na(Region))]]
+writexl::write_xlsx(dcHAC_lb, file.path(workdir, "temp/HAC2025 with live birth.xlsx"))
