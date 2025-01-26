4c9433ba636a216bf3b72477ca5b3912a6c39083
liuyanguu <lyhello@gmail.com>
2025-01-25 22:14:09 -0500

    add SDGRC regions 2025

--- script/get country_name.rds.R
+++ script/get country_name.rds.R
@@ -1,7 +1,8 @@
 # for saving some rds data
 
 library("data.table")
-# dt_sdmx <- fread("https://raw.githubusercontent.com/unicef-drp/DW-Production/refs/heads/dev/00_master/010_metadata/geographic_areas/geographic_areas.csv?token=GHSAT0AAAAAAC5OGJMWTORN4CN52DP33Y6GZ4VV74A")
+# copy from DW-Production/refs/heads/dev/00_master/010_metadata/geographic_areas/geographic_areas.csv
+# dt_sdmx <- fread("...") # copy the raw link
 # fwrite(dt_sdmx, "raw_data/SDMX_meta_info/geographic_areas.csv")
 
 dcname <- fread("raw_data/SDMX_meta_info/geographic_areas.csv")
