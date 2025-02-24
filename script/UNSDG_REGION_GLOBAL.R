5604cf59db5839ba440ca803cb1e02c7227241f1
liuyanguu <lyhello@gmail.com>
2025-02-23 22:13:53 -0500

    draft

--- script/UNSDG_REGION_GLOBAL.R
+++ script/UNSDG_REGION_GLOBAL.R
@@ -12,10 +12,7 @@ library("data.table")
 library("countrycode")
 source("R/general_functions.R")
 
-# YL: I haven't figured out how to pull from API, you are welcome to revise this
-# script
-
-dt_region <- fread("https://raw.githubusercontent.com/unicef-drp/DW-Production/refs/heads/dev/00_master/010_metadata/geographic_areas/regional_groups/countries.csv?token=GHSAT0AAAAAAC5OGJMXZ5JHMZO5WG2KZW7CZ5ZW2HA")
+# YL: I haven't figured out how to pull from API, draft for now
 
 dt_in <- fread("raw_data/UNSDG_REGION_GLOBAL/UNSDG_REGION_GLOBAL.csv")
 dt_in <- add.country.name(dt_in)
